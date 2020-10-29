b35397dc534231ef67f014f931f38604a0cab044
Yapar <39386194+Yapar@users.noreply.github.com>
2020-10-29 16:05:28 -0700

    Add files via upload

---
+++ Practica 5/Practica5.c
@@ -0,0 +1,138 @@
+
+#include <xc.h>
+#include <stdio.h>
+// Libraries/headers section
+//#include <Compiler.h>
+//::::::::    PIC18F45K50 CONFIGURATION BIT SETtINGS    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::----:----+
+// CONFIG1L                                                                                                                                  |
+#pragma config PLLSEL = PLL4X   // PLL Selection (4x clock multiplier)                                                                       |
+#pragma config CFGPLLEN = OFF   // PLL Enable Configuration bit (PLL Disabled (firmware controlled))                                         |
+#pragma config CPUDIV = NOCLKDIV// CPU System Clock Postscaler (CPU uses system clock (no divide))                                           |
+#pragma config LS48MHZ = SYS24X4// Low Speed USB mode with 48 MHz system clock (System clock at 24 MHz, USB clock divider is set to 4)
+
+// CONFIG1H
+#pragma config FOSC = INTOSCIO  // Oscillator Selection (Internal oscillator)
+#pragma config PCLKEN = ON      // Primary Oscillator Shutdown (Primary oscillator enabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config nPWRTEN = OFF    // Power-up Timer Enable (Power up timer disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable (BOR enabled in hardware (SBOREN is ignored))
+#pragma config BORV = 190       // Brown-out Reset Voltage (BOR set to 1.9V nominal)
+#pragma config nLPBOR = OFF     // Low-Power Brown-out Reset (Low-Power Brown-out Reset disabled)
+
+// CONFIG2H
+#pragma config WDTEN = OFF      // Watchdog Timer Enable bits (WDT disabled in hardware (SWDTEN ignored))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscaler (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = RC1     // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = ON      // PORTB A/D Enable bit (PORTB<5:0> pins are configured as analog input channels on Reset)
+#pragma config T3CMX = RC0      // Timer3 Clock Input MUX bit (T3CKI function is on RC0)
+#pragma config SDOMX = RB3      // SDO Output MUX bit (SDO function is on RB3)
+#pragma config MCLRE = ON       // Master Clear Reset Pin Enable (MCLR pin enabled; RE3 input disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset (Stack full/underflow will cause Reset)
+#pragma config LVP = ON         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled if MCLRE is also 1)
+#pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port Enable (ICPORT disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled)
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Block 0 Code Protect (Block 0 is not code-protected)
+#pragma config CP1 = OFF        // Block 1 Code Protect (Block 1 is not code-protected)
+#pragma config CP2 = OFF        // Block 2 Code Protect (Block 2 is not code-protected)
+#pragma config CP3 = OFF        // Block 3 Code Protect (Block 3 is not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protect (Boot block is not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protect (Data EEPROM is not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Block 0 Write Protect (Block 0 (0800-1FFFh) is not write-protected)
+#pragma config WRT1 = OFF       // Block 1 Write Protect (Block 1 (2000-3FFFh) is not write-protected)
+#pragma config WRT2 = OFF       // Block 2 Write Protect (Block 2 (04000-5FFFh) is not write-protected)
+#pragma config WRT3 = OFF       // Block 3 Write Protect (Block 3 (06000-7FFFh) is not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Registers Write Protect (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protect (Boot block (0000-7FFh) is not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protect (Data EEPROM is not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Block 0 Table Read Protect (Block 0 is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Block 1 Table Read Protect (Block 1 is not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Block 2 Table Read Protect (Block 2 is not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Block 3 Table Read Protect (Block 3 is not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protect (Boot block is not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+//                                                                                                                                           |
+//                                                                                                                            |
+//........    PIC18F45K50 configuration bit settings 
+
+
+// Directives section
+#define _XTAL_FREQ 1000000
+#define ONE_SECOND 1000
+
+// ISR for high-priority
+void __interrupt (high_priority ) high_isr ( void );
+
+//ISR for low-priority
+void __interrupt (low_priority) low_isr (void);
+
+// Data Type section
+
+char output=0, input=1;
+char digital= 0, analog = 1;
+char leds_OFF = 0, leds_ON = 1;
+char pushed = 0, no_pushed = 1;
+
+
+
+//Functions declaration
+void portsInit( void );  // Ports configuration
+
+// Main Section
+void main(void){
+    // Configuration
+    portsInit();
+    
+    //Infinite Cycle
+    while(1){
+        // If button is pushed then:
+        // Turn on the RA4 LED, otherwise turn off RA4 LED
+        
+        PORTBbits.RB4 = ~LATBbits.LATB4;
+        if(LATBbits.LATB4 == pushed) {  
+            LATAbits.LATA7 = leds_OFF;
+        } else {
+            LATAbits.LATA7 = leds_ON;
+        }
+
+        LATAbits.LATA7 = leds_ON;
+        __delay_ms(ONE_SECOND);     // Delay function XC8 Compiler
+        LATAbits.LATA7 = leds_OFF;
+        __delay_ms(ONE_SECOND);
+    }
+}
+
+// Functions Section
+
+//Ports configuration.
+void portsInit(void){
+    // Set PORTA as digital port also set RA4 and RA7 as output
+    ANSELA = digital;
+    TRISAbits.TRISA4 = output;
+    TRISAbits.TRISA7 = output;
+    
+    // Set PORTB as digital port also set RA4 as input 
+    ANSELB = digital;
+    TRISBbits.TRISB4 = input;
+
+}
